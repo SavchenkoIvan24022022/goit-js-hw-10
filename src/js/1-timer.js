4ea13f4de8ff7b1709be8dfc364e8b6ffd94f1e6
SavchenkoIvan24022022 <167643867+SavchenkoIvan24022022@users.noreply.github.com>
2024-10-04 20:32:18 +0300

    add timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,40 +1,52 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-let userSelectedDate = null; 
-
+const startBtn =  document.querySelector('[data-start]');
+const inputDate = document.querySelector('#datetime-picker')
+let intervalID = null;
+let userSelectedDate = null;
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
+
     onClose(selectedDates) {
-        userSelectedDate = selectedDates[0];
-        const now = new Date();
+    userSelectedDate = selectedDates[0];
+    const now = new Date();
+console.log(userSelectedDate);
+console.log(now);
+
 
         if (userSelectedDate < now) {
-            alert("Please choose a date in the future");
-            document.querySelector('[data-start]').disabled = true; 
+            iziToast.error({
+                title: 'Error',
+                message: 'Please choose a date in the future',
+                position: 'topRight',
+            });
+            startBtn.disabled = true; 
         } else {
-            document.querySelector('[data-start]').disabled = false; 
+            startBtn.disabled = false; 
         }
     },
 };
 
 flatpickr("#datetime-picker", options);
 
-document.querySelector('[data-start]').disabled = true; 
+ startBtn.disabled = true; 
 
-document.querySelector('[data-start]').addEventListener('click', startTimer);
+ startBtn.addEventListener('click', startTimer);
 
 function startTimer() {
     const targetDate = userSelectedDate.getTime(); 
 
-    clearInterval(window.timerInterval); 
-
-    window.timerInterval = setInterval(() => {
+    clearInterval(intervalID); 
+    inputDate.disabled = true;
+    startBtn.disabled = true; 
+    intervalID = setInterval(() => {
         const now = new Date().getTime();
         const remainingTime = targetDate - now;
 
@@ -43,16 +55,14 @@ function startTimer() {
         const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-    
-        document.querySelector('[data-days]').textContent = days; // Дні
+        document.querySelector('[data-days]').textContent = String(days).padStart(2, '0');
         document.querySelector('[data-hours]').textContent = String(hours).padStart(2, '0'); 
         document.querySelector('[data-minutes]').textContent = String(minutes).padStart(2, '0'); 
         document.querySelector('[data-seconds]').textContent = String(seconds).padStart(2, '0'); 
 
-        if (remainingTime < 0) {
-            clearInterval(window.timerInterval);
-            document.querySelector('.timer').innerHTML = '<span style="color: red;">Час вичерпано!</span>';
-            document.querySelector('[data-start]').disabled = true; 
+        if (remainingTime < 1000) {
+            clearInterval (intervalID);
+            inputDate.disabled = false; 
         }
     }, 1000);
 }
